f2a092641ab6a0bb98763837d83a846040cef593
Serkan Surguvec <serkansurguvec@gmail.com>
2025-07-15 11:46:11 +0300

    rejection resampling after image preprocessing

--- vision/vision_prep.py
+++ vision/vision_prep.py
@@ -211,7 +211,7 @@ print("Before resampling:", ratio.numpy())
 
 eq = tf.constant([1.0 / len(unique_labels)] * len(unique_labels), dtype=tf.float32)
 
-def class_func(a,b,md):
+def class_func(a,b,md,tr):
     return tf.argmax(b,axis=-1)
 
 zipped = (
